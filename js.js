9bfb765016085f9546e94f71c471378286c6602c
fabriscar <126738775+fabriscar@users.noreply.github.com>
2023-03-02 17:02:08 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+document.getElementById('nro').addEventListener('click', function() {
+  document.getElementById("cambio1").innerHTML = "MI NÚMERO DE TELÉFONO ES: (408) 798-2936";
+})
+document.getElementById('gmail').addEventListener('click',function () {
+  document.getElementById("cambio1").innerHTML="MI MAIL ES: jimmie.clark@example.com " 
+ });
+ document.getElementById('dir').addEventListener('click',function () {
+  document.getElementById("cambio1").innerHTML="MI DIRECCION ES: 7533 Royal Ln" 
+ });
+ document.getElementById('ed').addEventListener('click',function () {
+  document.getElementById("cambio1").innerHTML="MI FECHA DE NACIMIENTO ES: 10/5/1985" 
+ });
